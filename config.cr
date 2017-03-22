0342ff306be4966f3f6dc4bb86f598a8795c4edb
jakemoldham <jakemoldham@gmail.com>
2017-03-22 11:58:11 -0600

    initial commit with fez

---
+++ config.cr
@@ -0,0 +1,12 @@
+require "kemal"
+require "kilt/slang"
+
+require "./src/macros/*"
+require "./src/jarleigh.cr"
+
+File.read_lines(".env").each do |line|
+  key, value = line.strip.split "="
+  ENV[key] = value
+end
+
+
