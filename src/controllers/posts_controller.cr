5625688b85691547b5589d812635b5c9706ea35b
Arleigh Atkinson <arleigh.atkinson@nikolamotor.com>
2017-03-24 17:54:48 -0600

    Add some labels in controller for actions

--- src/controllers/posts_controller.cr
+++ src/controllers/posts_controller.cr
@@ -1,12 +1,15 @@
+# Index
 get "/posts" do
   posts = Post.all
   rend("index", "posts")
 end
 
+# New
 get "/posts/new" do
   rend("new", "posts")
 end
 
+# Create
 post "/posts" do |env|
   title = env.params.body["title"].as(String)
   body = env.params.body["title"].as(String)
@@ -16,17 +19,20 @@ post "/posts" do |env|
   env.redirect "/posts"
 end
 
+# Show
 get "/posts/:id" do |env|
   id = env.params.url["id"]
   post = Post.get(id)
   rend("show", "posts")
 end
 
+# Edit
 get "/post/edit/:id" do |env|
   post = Post.get(env.params.url["id"])
   rend("edit", "posts")
 end
 
+# Update
 get "/posts/update/:id" do |env|
   post = Post.get(env.params.url["id"])
   post.title = env.params.query["title"].to_s
@@ -35,6 +41,7 @@ get "/posts/update/:id" do |env|
   env.redirect "/posts/#{env.params.url["id"]}"
 end
 
+# Delete
 get "/posts/delete/:id" do |env|
   id = env.params.url["id"]
   post = Post.get(id)
