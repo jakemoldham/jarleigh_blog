05f12f6400325de367f70aef9024837542820e1e
Arleigh Atkinson <arleigh.atkinson@nikolamotor.com>
2017-03-24 11:45:26 -0600

    Add env variables for db

--- config.cr
+++ config.cr
@@ -10,6 +10,11 @@ require "./src/models/*"
 require "./src/jarleigh.cr"
 require "./src/controllers/*"
 
+ENV["MYSQL_HOST"] = "localhost"
+ENV["MYSQL_PORT"] = "3306"
+ENV["MYSQL_USER"] = "root"
+ENV["MYSQL_DATABASE"] = "jarleigh_blog"
+
 File.read_lines(".env").each do |line|
   key, value = line.strip.split "="
   ENV[key] = value
