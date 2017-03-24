573dd07fff1b5701bdc577d00c95ccf629710205
Arleigh Atkinson <arleigh.atkinson@nikolamotor.com>
2017-03-24 10:46:56 -0600

    My bad!
    all of the actions got duplicated...

--- src/controllers/posts_controller.cr
+++ src/controllers/posts_controller.cr
@@ -30,32 +30,3 @@ delete "/post/:id" do |env|
   post.delete
 end
 
-get "/posts/new" do
-  rend "posts/new"
-end
-
-get "/posts/create" do |env|
-  post = Post.create({ "title" => "#{env.params.query["title"].to_s}",
-                        "body" => "#{env.params.query["body"].to_s}",
-                    })
-  env.redirect "/posts"
-end
-
-get "/posts/:id" do |env|
-  id = env.params.url["id"]
-  post = Post.get(id)
-  rend "posts/show" 
-end
-
-put "/posts/edit/:id" do |env|
-  post = Post.get(env.params.url["id"])
-  post.title = env.params.query["title"].to_s
-  post.body = env.params.query["body"].to_s
-  post.update
-end
-
-delete "/post/delete/:id" do |env|
-  id = env.params.url["id"]
-  post = Post.get(id)
-  post.delete
-end
