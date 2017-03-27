f98031cd4dc2f14f8aa222c86de4760ad45e1d23
jakemoldham <jakemoldham@gmail.com>
2017-03-27 17:44:06 -0600

    front end and connections to both index and show pages

--- src/controllers/sites_controller.cr
+++ src/controllers/sites_controller.cr
@@ -1,3 +1,5 @@
 get "/" do |env|
+  posts = Post.all
+  spot = Post.where({ "spotlight" => true })
   rend "index"                   
 end
--- src/models/post.cr
+++ src/models/post.cr
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Model
   primary id                 : Int
   field title                : String
   field body                 : String
+  field author               : String
+  field body                 : String
+  field description          : String
+  field spotlight            : Int
 end
